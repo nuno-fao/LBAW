2dba9e70115cb4f2925895b539d11444f74e7959
up201806206@fe.up.pt <up201806206>
2021-03-07 20:45:54 +0000

    added sticky left side to user page

--- src/user_profile.php
+++ src/user_profile.php
@@ -6,8 +6,8 @@ draw_navbar_normal_user();
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <div class="col-lg-12 col-12 mt-5 row mx-auto">
-    <aside id="user_info" class="col-xl-4 col-lg-5 col-12">
-        <div class="card">
+    <aside id="user_info" class="col-xl-4 col-lg-5 col-12 fixed">
+        <div class="card mx-auto">
             <div class="card-body">
                 <div class="d-flex flex-column align-items-center text-center">
                     <img src="images/user_pic.png" alt="Admin" class="rounded-circle" width="150">
@@ -21,41 +21,75 @@ draw_navbar_normal_user();
                 </div>
             </div>
         </div>
-        <div class="row ms-auto me-auto my-4">
-            <h4>
+        <section>
+            <h4 class="mt-3 text-center">
                 Friends
             </h4>
-            <a href=" #" class="list-group-item list-group-item-action" aria-current="true">
-                <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1">Mickey Mouse</h5>
+            <section class="d-flex flex-lg-row flex-row">
+                <div class="row ms-auto me-auto my-4 mx-1">
+                    <a href=" #" class="list-group-item list-group-item-action" aria-current="true">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-1">Mickey Mouse</h5>
+                        </div>
+                        <p class="mb-1">@the_real_mickey</p>
+                    </a>
+                    <a href=" #" class="list-group-item list-group-item-action" aria-current="true">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-1">The Beast</h5>
+                        </div>
+                        <p class="mb-1">@im_the_beast</p>
+                    </a>
+                    <a href=" #" class="list-group-item list-group-item-action" aria-current="true">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-1">Beauty</h5>
+                        </div>
+                        <p class="mb-1">@im_a_beauty</p>
+                    </a>
+                    <a href=" #" class="list-group-item list-group-item-action" aria-current="true">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-1">Donald Duck</h5>
+                        </div>
+                        <p class="mb-1">@quack_like_champ</p>
+                    </a>
                 </div>
-                <p class="mb-1">@the_real_mickey</p>
-            </a>
-            <a href=" #" class="list-group-item list-group-item-action" aria-current="true">
-                <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1">The Beast</h5>
+                <div class="row ms-auto me-auto my-4 mx-1">
+                    <a href=" #" class="list-group-item list-group-item-action" aria-current="true">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-1">Sonic</h5>
+                        </div>
+                        <p class="mb-1">@fasterthanflash</p>
+                    </a>
+                    <a href=" #" class="list-group-item list-group-item-action" aria-current="true">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-1">Androis</h5>
+                        </div>
+                        <p class="mb-1">@imagreendroid</p>
+                    </a>
+                    <a href=" #" class="list-group-item list-group-item-action" aria-current="true">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-1">Sleepy Beaty</h5>
+                        </div>
+                        <p class="mb-1">@sleep4ever</p>
+                    </a>
+                    <a href=" #" class="list-group-item list-group-item-action" aria-current="true">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-1">Snopy</h5>
+                        </div>
+                        <p class="mb-1">@hatelife</p>
+                    </a>
                 </div>
-                <p class="mb-1">@im_the_beast</p>
-            </a>
-            <a href=" #" class="list-group-item list-group-item-action" aria-current="true">
-                <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1">Beauty</h5>
-                </div>
-                <p class="mb-1">@im_a_beauty</p>
-            </a>
-            <a href=" #" class="list-group-item list-group-item-action" aria-current="true">
-                <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1">Donald Duck</h5>
-                </div>
-                <p class="mb-1">@quack_like_champ</p>
-            </a>
-        </div>
+            </section>
+        </section>
     </aside>
-    <section class="col-xl-8 col-lg-7 col-12">
+    <section class="col-xl-8 col-lg-7 col-12 scrollit me-3 ms-auto">
         <h4 class="text-center">
             Latest Reviews
         </h4>
         <?php
+        draw_review_1();
+        draw_review_2();
+        draw_review_1();
+        draw_review_2();
         draw_review_1();
         draw_review_2();
         ?>
