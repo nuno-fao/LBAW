52afbd71df083bb828e1f2bde067aab0b8364fc4
up201806206@fe.up.pt <up201806206>
2021-03-11 10:46:23 +0000

    made notification in 1 line

--- src/notifications_page.php
+++ src/notifications_page.php
@@ -12,52 +12,42 @@ draw_breadcrumb($arr);
 
 ?>
 <div class="container d-flex flex-column">
-    <div class="display-5 text-center my-5">
+    <div class="display-5 text-center my-2">
         Pending Notifications
     </div>
 
-    <div class="card col-12 col-lg-6 no-padding mx-auto mt-3" id="request_0">
-        <div class="card-header bg-secondary text-primary">
-            Friend Request
-        </div>
-        <div class="card-body">
-            <p class="card-text"><a href="group_page.php">Beast</a> sent you a friend request</p>
+    <div class="card col-12 col-lg-7 no-padding mx-auto mt-3" id="request_0">
+        <div class="card-body d-flex justify-content-between">
+            <p class="card-text my-auto"><a href="group_page.php">Beast</a> sent you a friend request</p>
             <div class="d-flex justify-content-end">
                 <a href="#" class="btn btn-primary ms-3 request_button" id="request_0_accept">Accept</a>
                 <a href="#" class="btn btn-primary ms-3 request_button" id="request_0_decline">Decline</a>
             </div>
         </div>
     </div>
-    <div class="card col-12 col-lg-6 no-padding mx-auto mt-3" id="request_1">
-        <div class="card-header bg-primary text-light">
-            Group Request
-        </div>
-        <div class="card-body">
-            <p class="card-text"><a href="group_page.php">Movie Elitists</a> invited you to join their group</p>
+    <div class="card col-12 col-lg-7 no-padding mx-auto mt-3" id="request_1">
+        <div class="card-body  d-flex justify-content-between">
+            <p class="card-text my-auto"><a href="group_page.php">Movie Elitists</a> invited you to join their group
+            </p>
             <div class="d-flex justify-content-end">
-                <a href="#" class="btn btn-primary ms-3 request_button" id="request_1_accept">Accept</a>
+                <a href="#" class="btn btn-primary ms-3 request_button" id="request_1_accept">Join</a>
                 <a href="#" class="btn btn-primary ms-3 request_button" id="request_1_decline">Decline</a>
             </div>
         </div>
     </div>
-    <div class="card col-12 col-lg-6 no-padding mx-auto mt-3" id="request_2">
-        <div class="card-header bg-primary text-light">
-            Group Request
-        </div>
-        <div class="card-body">
-            <p class="card-text"><a href="group_page.php">Justice League</a> invited you to join their group</p>
+    <div class="card col-12 col-lg-7 no-padding mx-auto mt-3" id="request_2">
+        <div class="card-body  d-flex justify-content-between">
+            <p class="card-text  my-auto"><a href="group_page.php">Justice League</a> invited you to join their group
+            </p>
             <div class="d-flex justify-content-end">
                 <a href="#" class="btn btn-primary ms-3 request_button" id="request_2_accept">Accept</a>
                 <a href="#" class="btn btn-primary ms-3 request_button" id="request_2_decline">Decline</a>
             </div>
         </div>
     </div>
-    <div class="card col-12 col-lg-6 no-padding mx-auto mt-3" id="request_3">
-        <div class="card-header bg-secondary text-primary">
-            Friend Request
-        </div>
-        <div class="card-body">
-            <p class="card-text"><a href="group_page.php">Gervásio</a> sent you a friend request</p>
+    <div class="card col-12 col-lg-7 no-padding mx-auto mt-3" id="request_3">
+        <div class="card-body  d-flex justify-content-between">
+            <p class="card-text my-auto"><a href="group_page.php">Gervásio</a> sent you a friend request</p>
             <div class="d-flex justify-content-end">
                 <a href="#" class="btn btn-primary ms-3 request_button" id="request_3_accept">Accept</a>
                 <a href="#" class="btn btn-primary ms-3 request_button" id="request_3_decline">Decline</a>
