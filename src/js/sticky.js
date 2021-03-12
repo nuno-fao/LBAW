6b8deed646de5195983944d7bcb0d48ca5cb8e68
up201806206@fe.up.pt <up201806206>
2021-03-12 19:20:28 +0000

    addded aside fix to js null

--- src/js/sticky.js
+++ src/js/sticky.js
@@ -3,7 +3,9 @@ let aside = document.querySelector(".sticky_left_aside");
 let friends = document.querySelector("#down_section");
 let sec = document.querySelector("#collapsable_section");
 let navbar = document.querySelector("nav");
-let h = aside.clientHeight + navbar.clientHeight + 50;
+let h = 0
+if (aside !== null)
+    h = aside.clientHeight + navbar.clientHeight + 50;
 let removed = false;
 if (window.innerWidth >= 980 && window.innerHeight <= h && removed === false) {
     sec.removeChild(friends)
