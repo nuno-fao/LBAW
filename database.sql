b0c94edb167daa080f48c7e50c6a656206a71274
lmarques03 <henriquecondado@gmail.com>
2021-03-29 00:26:27 +0100

    Updated db, and populated about half of it

--- database.sql
+++ database.sql
@@ -1,3 +1,6 @@
+DROP TYPE IF EXISTS state CASCADE;
+CREATE TYPE state AS ENUM ('pending', 'accepted', 'rejected');
+
 DROP TABLE IF EXISTS movie CASCADE;
 CREATE TABLE movie
 (
@@ -27,13 +30,11 @@ CREATE TABLE movie_genre
     CONSTRAINT genre_key FOREIGN KEY (genre)
         REFERENCES genre (id) 
         ON UPDATE CASCADE
-        ON DELETE CASCADE
-        NOT VALID,
+        ON DELETE CASCADE,
     CONSTRAINT movie_key FOREIGN KEY (movie)
-        REFERENCES movie (id) 
+        REFERENCES public.movie (id) 
         ON UPDATE CASCADE
         ON DELETE CASCADE
-        NOT VALID
 );
 
 DROP TABLE IF EXISTS signed_user CASCADE;
@@ -43,30 +44,31 @@ CREATE TABLE signed_user
     photo text ,
     username text NOT NULL,
     name text NOT NULL,
-    age integer,
+    date_of_birth date,
     email text NOT NULL,
+    password text NOT NULL,
     admin boolean NOT NULL DEFAULT false,
     CONSTRAINT signed_user_pkey PRIMARY KEY (id),
     CONSTRAINT email_unique UNIQUE (email),
-    CONSTRAINT username_unique UNIQUE (username),
-    CONSTRAINT age_constraint CHECK (age > 13)
+    CONSTRAINT username_unique UNIQUE (username)
 );
 
 DROP TABLE IF EXISTS friend CASCADE;
-CREATE TABLE friend
+CREATE TABLE friend --trigger
 (
-    id1 integer NOT NULL,
-    id2 integer NOT NULL,
-    CONSTRAINT friend_pkey PRIMARY KEY (id1, id2),
-    CONSTRAINT friend_id1_fkey FOREIGN KEY (id1)
-        REFERENCES signed_user (id) 
+    signed_user_id1 integer NOT NULL,
+    signed_user_id2 integer NOT NULL,
+    friendship_state state NOT NULL DEFAULT 'pending',
+    CONSTRAINT friend_pkey PRIMARY KEY (signed_user_id1, signed_user_id2),
+    CONSTRAINT friend_id1_fkey FOREIGN KEY (signed_user_id1)
+        REFERENCES public.signed_user (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE,
-    CONSTRAINT friend_id2_fkey FOREIGN KEY (id2)
-        REFERENCES signed_user (id) 
+    CONSTRAINT friend_id2_fkey FOREIGN KEY (signed_user_id2)
+        REFERENCES public.signed_user (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE,
-    CONSTRAINT diferent_user_check CHECK (id1 <> id2)
+    CONSTRAINT diferent_user_check CHECK (signed_user_id1 <> signed_user_id2)
 );
 
 DROP TABLE IF EXISTS "group" CASCADE;
@@ -79,27 +81,26 @@ CREATE TABLE "group"
     admin integer NOT NULL,
     CONSTRAINT group_pkey PRIMARY KEY (id),
     CONSTRAINT admin_key FOREIGN KEY (id)
-        REFERENCES signed_user (id) 
+        REFERENCES public.signed_user (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );
 
-DROP TABLE IF EXISTS group_member CASCADE;
+DROP TABLE IF EXISTS "group_member" CASCADE;
 CREATE TABLE group_member
 (
-    "group" integer NOT NULL,
-    "user" integer NOT NULL,
-    CONSTRAINT group_member_pkey PRIMARY KEY ("group", "user"),
-    CONSTRAINT group_member_group_fkey FOREIGN KEY ("group")
-        REFERENCES "group" (id) 
+    "group_id" integer NOT NULL,
+    "user_id" integer NOT NULL,
+    membership_state state NOT NULL DEFAULT 'pending',
+    CONSTRAINT group_member_pkey PRIMARY KEY ("group_id", "user_id"),
+    CONSTRAINT group_member_group_fkey FOREIGN KEY ("group_id")
+        REFERENCES public."group" (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT group_member_user_fkey FOREIGN KEY ("user_id")
+        REFERENCES public.signed_user (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
-        NOT VALID,
-    CONSTRAINT group_member_user_fkey FOREIGN KEY ("user")
-        REFERENCES signed_user (id) 
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-        NOT VALID
 );
 
 DROP TABLE IF EXISTS review CASCADE;
@@ -108,22 +109,22 @@ CREATE TABLE review
     id serial NOT NULL ,
     title text  NOT NULL,
     text text NOT NULL,
-    date date NOT NULL DEFAULT now(),
+    date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "group" integer,
     movie integer NOT NULL,
     author integer NOT NULL,
     CONSTRAINT review_pkey PRIMARY KEY (id),
     CONSTRAINT review_group_movie_author_key UNIQUE ("group", movie, author),
     CONSTRAINT review_author_fkey FOREIGN KEY (author)
-        REFERENCES signed_user (id) 
+        REFERENCES public.signed_user (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE,
     CONSTRAINT review_group_fkey FOREIGN KEY ("group")
-        REFERENCES "group" (id) 
+        REFERENCES public."group" (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE,
     CONSTRAINT review_movie_fkey FOREIGN KEY (movie)
-        REFERENCES movie (id) 
+        REFERENCES public.movie (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );
@@ -135,11 +136,11 @@ CREATE TABLE "like"
     review integer NOT NULL,
     CONSTRAINT like_pkey PRIMARY KEY ("user", review),
     CONSTRAINT like_review_fkey FOREIGN KEY (review)
-        REFERENCES review (id) 
+        REFERENCES public.review (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE,
     CONSTRAINT like_user_fkey FOREIGN KEY ("user")
-        REFERENCES signed_user (id) 
+        REFERENCES public.signed_user (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );
@@ -151,13 +152,14 @@ CREATE TABLE comment
     text text NOT NULL,
     author integer NOT NULL,
     review integer NOT NULL,
+    date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT comment_pkey PRIMARY KEY (id),
     CONSTRAINT comment_author_fkey FOREIGN KEY (author)
-        REFERENCES signed_user (id) 
+        REFERENCES public.signed_user (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE,
     CONSTRAINT comment_review_fkey FOREIGN KEY (review)
-        REFERENCES review (id) 
+        REFERENCES public.review (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );
@@ -172,11 +174,11 @@ CREATE TABLE rating
     CONSTRAINT rating_pkey PRIMARY KEY (id),
     CONSTRAINT rating_user_movie_key UNIQUE ("user", movie),
     CONSTRAINT rating_movie_fkey FOREIGN KEY (movie)
-        REFERENCES movie (id) 
+        REFERENCES public.movie (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE,
     CONSTRAINT rating_user_fkey FOREIGN KEY ("user")
-        REFERENCES signed_user (id) 
+        REFERENCES public.signed_user (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE,
     CONSTRAINT rating_rating_check CHECK (rating > 0 AND rating <= 10)
@@ -186,59 +188,27 @@ DROP TABLE IF EXISTS notifications CASCADE;
 CREATE TABLE notifications
 (
     id serial NOT NULL,
-    date date NOT NULL DEFAULT now(),
+    date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     recipient integer NOT NULL,
+    group_invite integer,
+    friend_invite integer,
+    reported_review integer,
     CONSTRAINT notifications_pkey PRIMARY KEY (id),
+    CONSTRAINT notifications_friend_invite_fkey FOREIGN KEY (friend_invite)
+        REFERENCES public.signed_user (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT notifications_group_invite_fkey FOREIGN KEY (group_invite)
+        REFERENCES public."group" (id) 
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
     CONSTRAINT notifications_recipient_fkey FOREIGN KEY (recipient)
-        REFERENCES signed_user (id) 
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-);
-
-DROP TABLE IF EXISTS friend_invite CASCADE;
-CREATE TABLE friend_invite
-(
-    id1 integer NOT NULL,
-    "from" integer NOT NULL,
-    CONSTRAINT friend_invite_pkey PRIMARY KEY (id1),
-    CONSTRAINT friend_invite_from_fkey FOREIGN KEY ("from")
-        REFERENCES signed_user (id) 
+        REFERENCES public.signed_user (id) 
         ON UPDATE CASCADE
         ON DELETE CASCADE,
-    CONSTRAINT friend_invite_id1_fkey FOREIGN KEY (id1)
-        REFERENCES notifications (id) 
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-);
-
-DROP TABLE IF EXISTS group_invite CASCADE;
-CREATE TABLE group_invite
-(
-    id1 integer NOT NULL,
-    "from" integer NOT NULL,
-    CONSTRAINT group_invite_pkey PRIMARY KEY (id1),
-    CONSTRAINT group_invite_from_fkey FOREIGN KEY ("from")
-        REFERENCES "group" (id) 
+    CONSTRAINT notifications_reported_review_fkey FOREIGN KEY (reported_review)
+        REFERENCES public.review (id) 
         ON UPDATE CASCADE
         ON DELETE CASCADE,
-    CONSTRAINT group_invite_id1_fkey FOREIGN KEY (id1)
-        REFERENCES notifications (id) 
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-);
-
-DROP TABLE IF EXISTS review_report CASCADE;
-CREATE TABLE review_report
-(
-    id1 integer NOT NULL,
-    "from" integer NOT NULL,
-    CONSTRAINT review_report_pkey PRIMARY KEY (id1),
-    CONSTRAINT review_report_from_fkey FOREIGN KEY ("from")
-        REFERENCES review (id) 
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
-    CONSTRAINT review_report_id1_fkey FOREIGN KEY (id1)
-        REFERENCES notifications (id) 
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
+    CONSTRAINT notifications_check CHECK (group_invite IS NULL AND friend_invite IS NULL AND reported_review IS NOT NULL OR group_invite IS NULL AND friend_invite IS NOT NULL AND reported_review IS NULL OR group_invite IS NOT NULL AND friend_invite IS NULL AND reported_review IS NULL) NOT VALID
 );
