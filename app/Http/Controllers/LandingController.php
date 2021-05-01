320aab1dd03f31aeff35c62195bf6024bc3baedc
lmarques03 <henriquecondado@gmail.com>
2021-05-01 20:19:06 +0100

    Main page now getting movies from db

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class LandingController extends Controller
 {
     public function index(){
-        return view('landing_page');
+
+        $movies = Movie::get();
+        return view('landing_page', [
+            'movies' => $movies,
+        ]);
     }
 }
