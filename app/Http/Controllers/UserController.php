e598fb4cc347aa013679a9e7b3f3d57408a4c0e1
lmarques03 <henriquecondado@gmail.com>
2021-05-10 12:30:24 +0100

    Only the user's reviews show in his personal page now

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function show(User $user){
 
-        $reviews = Review::get();
+        $reviews = $user->reviews()->with(['user'])->paginate(10);
         
         return view('pages.user_profile', [
             'user' => $user,
