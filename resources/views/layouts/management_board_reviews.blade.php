3f6819774de4f7f4583745280e79d02c63ec99c0
lmarques03 <henriquecondado@gmail.com>
2021-05-28 19:38:29 +0100

    Added sidebar templates for administration dashboard

---
+++ resources/views/layouts/management_board_reviews.blade.php
@@ -0,0 +1,39 @@
+<aside id="user_info" class="bg-light border col-xl-2 col-lg-2 col-8 mx-lg-0 mx-auto d-flex flex-column">
+    <h5 class="mx-auto mt-3 display-6">Board</h5>
+    <a class="btn btn-primary mt-3" href="movies_board.php">
+        <h4 class="">Movies</h4>
+    </a>
+    <button class="btn btn-primary mt-3" disabled>
+        <h4 class="">Reported</h4>
+    </button>
+    <a class="btn btn-primary mt-3" href="users_board.php">
+        <h4 class="">Users</h4>
+    </a>
+    <div class="mt-3 d-flex flex-column container">
+        <strong class="mb-3">Reviews Published</strong>
+        <strong>
+            Total
+        </strong>
+        <span>
+            <p class="mx-auto">
+                103
+            </p>
+        </span>
+        <strong>
+            This week
+        </strong>
+        <span>
+            <p class="mx-auto">
+                10
+            </p>
+        </span>
+        <strong>
+            Today
+        </strong>
+        <span>
+            <p class="mx-auto">
+                3
+            </p>
+        </span>
+    </div>
+</aside>
---
+++ resources/views/layouts/management_board_user.blade.php
@@ -0,0 +1,12 @@
+<aside id="user_info" class="bg-light border col-xl-2 col-lg-2 col-8 mx-lg-0 mx-auto d-flex flex-column">
+    <h5 class="mx-auto mt-3 display-6">Board</h5>
+    <a class="btn btn-primary mt-3" href="movies_board.php">
+        <h4 class="">Movies</h4>
+    </a>
+    <a class="btn btn-primary mt-3" href="review_board.php">
+        <h4 class="">Reported</h4>
+    </a>
+    <button class="btn btn-primary mt-3" disabled>
+        <h4 class="">Users</h4>
+    </button>
+</aside>
