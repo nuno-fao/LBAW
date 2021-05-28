3f97efc174706cd642c2f3fa3931171e81859956
lmarques03 <henriquecondado@gmail.com>
2021-05-28 19:45:19 +0100

    Added movie dashboard page

---
+++ resources/views/pages/movies_dashboard.blade.php
@@ -0,0 +1,157 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="col-lg-12 col-12 row mx-auto">
+    
+    <section class="col-xl-7 col-lg-6 col-12 scrollit me-auto ms-auto">
+        <h4 class="mt-3 text-center mb-5">
+            Movies
+        </h4>
+        <section class="d-flex flex-column mx-auto">
+            <div class="col-12  mx-auto my-2">
+                <div class="row">
+                    <div class="col-lg-3 col-6 mx-auto mt-1">
+                        <a class="card  no-padding" href="movie_page.php">
+                            <div class="card-body d-flex flex-column d-flex justify-content-center fs-6 gap-0 p-3">
+                                <p class="card-text text-center mb-0 text-truncate">Django Unchained</p>
+                                <p class="card-text text-center mt-0">(2012)</p>
+                            </div>
+                            <img src="images/djangounchainedposter.jpg" class="card-img-top" alt="...">
+                            
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-6 mx-auto mt-1">
+                        <a class="card  no-padding" href="movie_page.php">
+                            <div class="card  no-padding">
+                                <div class="card-body d-flex flex-column d-flex justify-content-center fs-6 gap-0 p-3">
+                                    <p class="card-text text-center mb-0 text-truncate">Fight Club</p>
+                                    <p class="card-text text-center mt-0">(1999)</p>
+                                </div>
+                                <img src="images/fightclubposter.jpg" class="card-img-top" alt="...">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-6 mx-auto mt-1">
+                        <a class="card  no-padding" href="movie_page.php">
+                            <div class="card  no-padding">
+                                <div class="card-body d-flex flex-column d-flex justify-content-center fs-6 gap-0 p-3">
+                                    <p class="card-text text-center mb-0 text-truncate">Inception</p>
+                                    <p class="card-text text-center mt-0">(2010)</p>
+                                </div>
+                                <img src="images/inceptionposter.jpg" class="card-img-top" alt="...">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-6 mx-auto mt-1">
+                        <a class="card  no-padding" href="movie_page.php">
+                            <div class="card  no-padding">
+                                <div class="card-body d-flex flex-column d-flex justify-content-center fs-6 gap-0 p-3">
+                                    <p class="card-text text-center mb-0 text-truncate">V for Vendetta</p>
+                                    <p class="card-text text-center mt-0">(2005)</p>
+                                </div>
+                                <img src="images/vforvendettaposter.jpg" class="card-img-top" alt="...">
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-3 col-6 mx-auto mt-1">
+                        <a class="card  no-padding" href="movie_page.php">
+                            <div class="card  no-padding">
+                                <div class="card-body d-flex flex-column d-flex justify-content-center fs-6 gap-0 p-3">
+                                    <p class="card-text text-center mb-0 text-truncate">Interstellar</p>
+                                    <p class="card-text text-center mt-0">(2014)</p>
+                                </div>
+                                <img src="images/interstellarposter.jpg" class="card-img-" alt="...">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-6 mx-auto mt-1">
+                        <a class="card  no-padding" href="movie_page.php">
+                            <div class="card  no-padding">
+                                <div class="card-body d-flex flex-column d-flex justify-content-center fs-6 gap-0 p-3">
+                                    <p class="card-text text-center mb-0 text-truncate">Lion King</p>
+                                    <p class="card-text text-center mt-0">(1994)</p>
+                                </div>
+                                <img src="images/lionkingposter.jpg" class="card-img-" alt="...">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-6 mx-auto mt-1">
+                        <a class="card  no-padding" href="movie_page.php">
+                            <div class="card  no-padding">
+                                <div class="card-body d-flex flex-column d-flex justify-content-center fs-6 gap-0 p-3">
+                                    <p class="card-text text-center mb-0 text-truncate">Pulp Fiction</p>
+                                    <p class="card-text text-center mt-0">(1994)</p>
+                                </div>
+                                <img src="images/pulpfiction.jpg" class="card-img-" alt="...">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-6 mx-auto mt-1">
+                        <a class="card  no-padding" href="movie_page.php">
+                            <div class="card  no-padding">
+                                <div class="card-body d-flex flex-column d-flex justify-content-center fs-6 gap-0 p-3">
+                                    <p class="card-text text-center mb-0 text-truncate">The Lord of the Rings The Return of the King</p>
+                                    <p class="card-text text-center mt-0 ">(2003)</p>
+                                </div>
+                                <img src="images/lordoftheringsposter.jpg" class="card-img-" alt="...">
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 col-6 mx-auto mt-1">
+                        <a class="card  no-padding" href="movie_page.php">
+                            <div class="card  no-padding">
+                                <div class="card-body d-flex flex-column d-flex justify-content-center fs-6 gap-0 p-3">
+                                    <p class="card-text text-center mb-0 text-truncate">Forrest Gump</p>
+                                    <p class="card-text text-center mt-0">(1994)</p>
+                                </div>
+                                <img src="images/forrestgumpposter.jpg" class="card-img-" alt="...">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-6 mx-auto mt-1">
+                        <a class="card  no-padding" href="movie_page.php">
+                            <div class="card  no-padding">
+                                <div class="card-body d-flex flex-column d-flex justify-content-center fs-6 gap-0 p-3">
+                                    <p class="card-text text-center mb-0 text-truncate">Toy Story</p>
+                                    <p class="card-text text-center mt-0">(1995)</p>
+                                </div>
+                                <img src="images/toystoryposter.jpg" class="card-img-" alt="...">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-6 mx-auto mt-1">
+                        <a class="card  no-padding" href="movie_page.php">
+                            <div class="card  no-padding">
+                                <div class="card-body d-flex flex-column d-flex justify-content-center fs-6 gap-0 p-3">
+                                    <p class="card-text text-center mb-0 text-truncate">Thor</p>
+                                    <p class="card-text text-center mt-0">(2011)</p>
+                                </div>
+                                <img src="images/thorposter.jpg" class="card-img-" alt="...">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-6 mx-auto mt-1">
+                        <a class="card  no-padding" href="movie_page.php">
+                            <div class="card  no-padding">
+                                <div class="card-body d-flex flex-column d-flex justify-content-center fs-6 gap-0 p-3">
+                                    <p class="card-text text-center mb-0 text-truncate">Shawshank Redemption</p>
+                                    <p class="card-text text-center mt-0">(1994)</p>
+                                </div>
+                                <img src="images/shawshankposter.jpg" class="card-img-" alt="...">
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    </section>
+</div>
+
+@endsection
