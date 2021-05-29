f89bb320e5ed57e2035192f5c5d4e8826684ab65
lmarques03 <henriquecondado@gmail.com>
2021-05-29 12:35:39 +0100

    Added Report Model

---
+++ app/Models/Report.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Report extends Model
+{
+    use HasFactory;
+    protected $primaryKey = 'id';
+    protected $table = 'report';
+    public $incrementing = true;
+    public $timestamps = false;
+
+    protected $fillable = [
+        'signed_user_id',
+        'review_id'
+    ];
+
+    public function getReview(){
+        return Review::find($review_id));
+    }
+}
