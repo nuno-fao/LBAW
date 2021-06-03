d35429a24f7851b9df2f7f2538b18e9ec528227f
lmarques03 <henriquecondado@gmail.com>
2021-06-03 19:48:30 +0100

    Fixed image size in movie list

--- resources/views/partials/movie_card.blade.php
+++ resources/views/partials/movie_card.blade.php
@@ -1,5 +1,5 @@
 <div class="col card col-lg-3 col-md-6 col-12 card mb-3">
-    <img class="card-img-top img-responsive" src="{{ asset($movie->photo) }}" alt="poster">
+    <img class="card-img-top img-responsive" src="{{ asset($movie->photo) }}" alt="poster" style="height:380px;">
     <div class="card-body">
         <h5 class="card-title text-nowrap">
             <a href="{{ url("/movie/{$movie->id}") }}"
