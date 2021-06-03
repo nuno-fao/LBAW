87df80b82c21791021d5d324c097bd87d2d2ed7b
lmarques03 <henriquecondado@gmail.com>
2021-06-03 16:53:04 +0100

    Password recovery done

--- app/Http/Controllers/ForgotPasswordController.php
+++ app/Http/Controllers/ForgotPasswordController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
 
 class ForgotPasswordController extends Controller
@@ -14,12 +16,54 @@ class ForgotPasswordController extends Controller
 
     }
 
-    public function forgot() {
+    public function reset_page() {
+       
+        return view('auth.reset_password');
 
-        
-        $credentials = request()->validate(['email' => 'required|email']);
-        Password::sendResetLink($credentials);
-
-        return response()->json(["msg" => 'Reset password link sent on your email id.']);
     }
+
+    public function sendResetLinkEmail(Request $request) {
+
+        $user = User::where('email', $request->get('email'))->first();
+
+        if (!is_null($user)) {
+            $response = Password::sendResetLink(
+                ['email' => $request->get('email')]
+            );
+        } else {
+            $response = Password::INVALID_USER;
+        }
+
+        return $response == Password::RESET_LINK_SENT
+            ? redirect()->route('confirmation_page')
+            : response()->json($response);
+    }
+
+    public function reset() {
+
+    
+        $credentials = request()->validate([
+            'email' => 'required|email',
+            'token' => 'required|string',
+            'password' => 'required|confirmed'
+        ]);
+
+
+        $reset_password_status = Password::reset($credentials, function ($user, $password) {
+            $user->password = Hash::make($password);
+            $user->save();
+        });
+
+        if ($reset_password_status == Password::INVALID_TOKEN) {
+            return response()->json(["msg" => "Invalid token provided"], 400);
+        }
+
+        return redirect()->route('login');
+    }
+
+    public function confirmation_page(){
+
+        return view('auth.confirmation_page');
+    }
+
 }
